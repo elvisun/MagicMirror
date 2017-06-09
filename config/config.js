0890af29a2a2e0e9870217a5e8c1e9e2ddbb6625
Elvis Sun <elvissunlihe@gmail.com>
2017-06-09 13:25:40 -0400

    edit config

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ var config = {
 	            position: 'bottom_left',
 	            header: 'Email',
 	            config:{
-	                user: 'elvissunlihe@gmail.com',
+	                user: 'summerqiu7@gmail.com',
 	                password: '123',
 	                host: 'imap.gmail.com',
 	                port: 993,
@@ -24,7 +24,8 @@ var config = {
 	                authTimeout: 10000,
 	                numberOfEmails: 5,
 	                fade: true,
-			maxCharacters: 30
+				maxCharacters: 30
+			}
         },
 		{
 			module: 'alert',
@@ -59,18 +60,31 @@ var config = {
 				    "morning" : [
 				        "Good morning, Summer!",
 				        "Who needs coffee when you have your smile?",
-				        "Have a lovely day honey!"
+				        "Have a lovely day honey!",
+				        "I'm not a photographer, but I can picture me and you together.",
+				        "Did you invent the airplane? Cause you seem Wright for me. ",
+				        "Is there an airport nearby or is that just my heart taking off?"
 				    ],
 				    "afternoon" : [
 				        "You are making a difference!",
 				        "Hi there Miss.beautiful!",
-				        "I miss you already!"
+				        "I miss you already!",
+				        "Excuse me, is your name Earl Grey? Because you look like a hot-tea!",
+				        "Are you an interior decorator? Because when I saw you, the entire room became beautiful.",
+				        "Are you a parking ticket? 'Cause you've got fine written all over you.",
+				        
+				        
 				    ],
 				    "evening" : [
 				        "You must be an amazing thief, you stole my heart already.",
-				        "Hi, Sexy!",
-				        "Your lips look lonely, wanna meet mine?",
-				        "Happy Valentines Day!"
+				        "Hi, beautiful!",
+				        "Are you a camera? Because every time I look at you, I smile.",
+				        "Are you a magician? Because whenever I look at you, everyone else disappears!",
+				        "If you were a vegetable you'd be a cute-cumber.",
+				        "Do you play soccer? Because you're a keeper!",
+				        "Do you have a map? I'm getting lost in your eyes.",
+				        "I don't have a library card, but do you mind if I check you out?",
+				        'You are now seeing the picture of the most beautiful girl (this one is original)'
 			    	   ]
 				}
 			}
